9f45a97c6fe1d59117c9be9ef7e4061b06bb9b95
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-18 03:34:34 +0000

    feat: implemented proxy contract

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xCe27E2ABcd3F163FdF0B46BF4e4D6A76977Ef30e'
+export const ProxyAdmin = '0x0576aEaF5779fc832D54531b32596f56c274CD31'
+export const SWTRImplementation = '0x12b2aC19352310E12a42A0DeD76aEfa7BA110Bfc'
+    
